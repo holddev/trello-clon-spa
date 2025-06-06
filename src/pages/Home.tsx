d6163c444cc3ccfb41b551280fa3a0af1f9eed16
Usuario6842 <136459003+Usuario6842@users.noreply.github.com>
2025-06-06 18:21:06 -0400

    feat: add home page with header, main content and footer

---
+++ src/pages/Home.tsx
@@ -0,0 +1,30 @@
+import { ArrowRightIcon } from "lucide-react"
+import { Button } from "../components/UI/Button"
+import { Icons } from "../components/Icons"
+import { Header } from "../components/Header"
+import { Footer } from "../components/Footer"
+
+export const Home = () => {
+  return (
+    <div className="max-w-5xl mx-auto px-2">
+      <Header />
+      <main className="flex items-center flex-col md:flex-row gap-5 min-h-screen pt-32 md:pt-0">
+        <div className="flex flex-col gap-4 sm:gap-5">
+          <h1 className="font-bold text-2xl sm:text-4xl md:text-5xl md:pb-2">Organiza tu trabajo y tu vida con <span className="text-blue-600 whitespace-nowrap">Trello Clon</span></h1>
+          <p className="text-pretty text-sm sm:text-base md:text-xl">
+            La herramienta de colaboración visual que libera el potencial de tu equipo para crear proyectos extraordinarios.
+          </p>
+          <Button
+            className="w-fit flex items-center gap-1 font-semibold"
+          >
+            Comenzar <ArrowRightIcon className="size-4" />
+          </Button>
+        </div>
+        <div>
+          <Icons icon="personWorking" className="h-auto min-w-[200px] w-[40vw] max-w-[300px]" />
+        </div>
+      </main>
+      <Footer />
+    </div>
+  )
+}
