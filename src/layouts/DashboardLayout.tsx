9b87bd62ab0fd9867ce342fd536ad245bde09ae3
Usuario6842 <136459003+Usuario6842@users.noreply.github.com>
2025-06-09 16:38:55 -0400

    refactor(layout): wrap dashboard layout with BoardProvider

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -1,17 +1,20 @@
 import { Outlet } from "react-router-dom"
 import { NavBar } from "../components/navigation/NavBar"
 import { SideBar } from "../components/SideBar"
+import { BoardProvider } from "../contexts/BoardProvider"
 
 export const DashboardLayout = () => {
   return (
-    <div className="h-auto flex max-h-screen overflow-hidden">
-      <SideBar />
-      <main className="w-full bg-background overflow-hidden">
-        <NavBar />
-        <div className="h-full overflow-y-scroll ">
-          <Outlet />
-        </div>
-      </main>
+    <div className="h-auto flex">
+      <BoardProvider>
+        <SideBar />
+        <main className="w-full bg-background max-h-screen overflow-hidden">
+          <NavBar />
+          <div className="h-full overflow-y-auto">
+            <Outlet />
+          </div>
+        </main>
+      </BoardProvider>
     </div>
   )
 }
