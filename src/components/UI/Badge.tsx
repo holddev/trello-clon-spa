5e3cdb536912f8423fcadd84f09e9f5e7162f74f
Usuario6842 <136459003+Usuario6842@users.noreply.github.com>
2025-06-12 20:13:29 -0400

    feat(Badge): add style prop to allow custom styling

--- src/components/UI/Badge.tsx
+++ src/components/UI/Badge.tsx
@@ -3,11 +3,15 @@ import { cn } from "../../utils/utils"
 interface Props {
   className?: string
   children: React.ReactNode
+  style?: React.CSSProperties
 }
 
-export const Badge = ({ className, children }: Props) => {
+export const Badge = ({ className, style, children }: Props) => {
   return (
-    <span className={cn("bg-gray-500 px-2 py-1 rounded-md", className)}>
+    <span
+      className={cn("bg-gray-500 px-2 py-1 rounded-md", className)}
+      style={style}
+    >
       {children}
     </span>
   )
