2c767793d055028e1a79be79a6ea729efc9d3e01
Usuario6842 <136459003+Usuario6842@users.noreply.github.com>
2025-06-18 19:27:14 -0400

    feat(auth): add clerk authentication to header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,22 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react"
 import { Button } from "./UI/Button"
+import { LogInIcon } from "lucide-react"
 
 export const Header = () => {
   return (
     <header className="fixed left-0 w-full  backdrop-blur-2xl shadow-sm">
       <div className="max-w-5xl flex justify-between py-2 mx-auto">
         <h2 className="text-base sm:text-xl md:text-2xl font-bold text-blue-600">Trello Clon</h2>
-        <Button className="font-semibold">Ingresar</Button>
+        <SignedOut>
+          <SignInButton mode="modal">
+            <Button className="font-semibold flex items-center gap-1">
+              <LogInIcon className="size-4" />Acceder
+            </Button>
+          </SignInButton>
+        </SignedOut>
+        <SignedIn>
+          <UserButton showName />
+        </SignedIn>
       </div>
     </header>
   )
