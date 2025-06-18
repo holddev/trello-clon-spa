6b6e0b08f6e8de293e5712deab5f587d9eed3381
Usuario6842 <136459003+Usuario6842@users.noreply.github.com>
2025-06-18 19:24:55 -0400

    feat(auth): add Clerk provider for authentication and protected routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,33 @@ import { Home } from "./pages/Home"
 import { Board } from "./pages/Board"
 import { DashboardLayout } from "./layouts/DashboardLayout"
 import { BoardDetail } from "./pages/BoardDetail"
+import { ClerkProvider } from "@clerk/clerk-react"
+import ProtectedRoute from "./components/ProtectedRoute"
 
 function App() {
+  const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+  console.log(PUBLISHABLE_KEY)
+  console.log(import.meta.env) // No muestra tu variable
+
+  if (!PUBLISHABLE_KEY) {
+    throw new Error('Add your Clerk Publishable Key to the .env file')
+  }
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<DashboardLayout />}>
-          <Route index element={<Board />} />
-          <Route path=":id" element={<BoardDetail />} />
-        </Route>
-      </Routes>
-    </Router>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={
+            <ProtectedRoute>
+              <DashboardLayout />
+            </ProtectedRoute>
+          }>
+            <Route index element={<Board />} />
+            <Route path=":id" element={<BoardDetail />} />
+          </Route>
+        </Routes>
+      </Router>
+    </ClerkProvider>
   )
 }
 
