702330b916e246ffe1b18c33498b39cf9904b903
Usuario6842 <136459003+Usuario6842@users.noreply.github.com>
2025-07-04 17:02:30 -0400

    feat(context): update BoardContext to use Board and ReorderBoard types

--- src/contexts/BoardContext.tsx
+++ src/contexts/BoardContext.tsx
@@ -1,12 +1,12 @@
 import { createContext } from "react";
-import type { TaskBoard } from "../types/types";
+import type { Board, ReorderBoard } from "../types/types";
 
 interface BoardContextProps {
-  boards: TaskBoard[]
-  addBoard: (board: TaskBoard) => void
+  boards: Board[]
+  addBoard: (board: Board) => void
   removeBoard: ({ id }: { id: number }) => void
-  updateBoard: (board: TaskBoard) => void
-  reorderBoard: (boards: TaskBoard[]) => void
+  updateBoard: (board: Board) => void
+  reorderBoard: (boards: ReorderBoard[]) => void
 }
 
 export const BoardContext = createContext<BoardContextProps | null>(null)
