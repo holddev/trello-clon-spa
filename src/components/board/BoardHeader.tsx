a2dc35993fbed27d269f59686b65b63d452fd2f1
Usuario6842 <136459003+Usuario6842@users.noreply.github.com>
2025-07-09 17:09:43 -0400

    fix(BoardHeader): update board type and properties for favorite status

--- src/components/board/BoardHeader.tsx
+++ src/components/board/BoardHeader.tsx
@@ -1,10 +1,10 @@
 
 import { Button } from "../UI/Button"
 import { Icons } from "../Icons"
-import type { TaskBoard } from "../../types/types"
+import type { BoardDetails } from "../../types/types"
 
 interface Props {
-  board: TaskBoard
+  board: BoardDetails
   onStar: (star: boolean) => void
 }
 
@@ -13,11 +13,11 @@ export const BoardHeader = ({ board, onStar }: Props) => {
     <div className="py-2 pl-12 pr-2 flex items-center gap-4 bg-gradient-to-br from-primary to-violet-500">
       <h3 className="text-xl font-semibold text-white/90" >{board.title}</h3>
       <Button
-        onClick={() => onStar(!board.isStarred)}
+        onClick={() => onStar(!board.is_favorite)}
         className="flex gap-2 items-center"
       >
-        <Icons icon={board.isStarred ? "starFilled" : "star"} className="size-4" />
-        {board.isStarred ? "Destacado" : "Destacar"}
+        <Icons icon={board.is_favorite ? "starFilled" : "star"} className="size-4" />
+        {board.is_favorite ? "Destacado" : "Destacar"}
       </Button>
     </div>
   )
