bd7aa40bbf8f1fa0b1f4df7005a04663d771199d
Usuario6842 <136459003+Usuario6842@users.noreply.github.com>
2025-07-11 18:29:31 -0400

    refactor(BoardHeader): update props to use boardId and fetch board details internally

--- src/components/board/BoardHeader.tsx
+++ src/components/board/BoardHeader.tsx
@@ -1,24 +1,32 @@
 
 import { Button } from "../UI/Button"
 import { Icons } from "../Icons"
-import type { BoardDetails } from "../../types/types"
+import { useBoard } from "../../hooks/useBoard"
 
 interface Props {
-  board: BoardDetails
+  boardId: number
   onStar: (star: boolean) => void
 }
 
-export const BoardHeader = ({ board, onStar }: Props) => {
+export const BoardHeader = ({ boardId, onStar }: Props) => {
+  const { boards } = useBoard()
+  const board = boards.find((b) => b.id == boardId)
+
   return (
     <div className="py-2 pl-12 pr-2 flex items-center gap-4 bg-gradient-to-br from-primary to-violet-500">
-      <h3 className="text-xl font-semibold text-white/90" >{board.title}</h3>
-      <Button
-        onClick={() => onStar(!board.is_favorite)}
-        className="flex gap-2 items-center"
-      >
-        <Icons icon={board.is_favorite ? "starFilled" : "star"} className="size-4" />
-        {board.is_favorite ? "Destacado" : "Destacar"}
-      </Button>
+      <h3 className="text-xl font-semibold text-white/90" >
+        {board ? board.title : '...'}
+      </h3>
+      {
+        board && (
+          <Button
+            onClick={() => board && onStar(!board.is_favorite)}
+            className="flex gap-2 items-center"
+          >
+            {<Icons icon={board.is_favorite ? "starFilled" : "star"} className="size-4" />}
+            {board.is_favorite ? "Destacado" : "Destacar"}
+          </Button>
+        )}
     </div>
   )
 }
