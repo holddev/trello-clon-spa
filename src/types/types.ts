97be407db56aa9e8b2aefaa5ddd6c157ad255149
Usuario6842 <136459003+Usuario6842@users.noreply.github.com>
2025-07-12 17:42:58 -0400

    feat(types): Update interfaces and types for boards, columns, and tasks

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,32 +1,34 @@
 export interface Tag {
-  name: string
-  color: string
+  id?: number;
+  task_id?: number;
+  text: string;
+  color: string;
 }
 
 export interface Task {
-  id?: number,
-  title: string,
-  tags: Tag[],
-  createdAt: Date,
-  description: string,
-  isOptimistic?: boolean
+  id: number;
+  column_id: number;
+  title: string;
+  description: string | null;
+  position: number;
+  created_at: string | null;
+  tags: Tag[];
+  isOptimistic?: boolean;
 }
 
-export interface TaskColumn {
-  id: number
-  title: string
-  tasks: Task[]
+export type newTask = Pick<Task, 'column_id' | 'title' | 'description' | 'position' | 'tags'>
+
+export interface Column {
+  id: number;
+  board_id: number;
+  title: string;
+  position: number;
+  created_at: string | null;
+  updated_at: string | null;
+  tasks: Task[];
 }
 
-export interface TaskBoard {
-  id: number
-  title: string
-  isStarred: boolean
-  columns: TaskColumn[]
-  createdAt: Date
-  updatedAt: Date
-  isOptimistic?: boolean
-}
+export type newColumn = Pick<Column, 'title' | 'board_id' | 'position'>
 
 
 export type ReorderType = {
@@ -41,4 +43,36 @@ export type ReorderType = {
   toTasks: Task[]
 }
 
+export interface Response<T> {
+  ok: boolean
+  message?: string
+  data: T
+  status: number
+}
 
+export interface Board {
+  id: number;
+  user_id: string;
+  title: string;
+  is_favorite: boolean;
+  order: number;
+  created_at: string;
+  isOptimistic?: boolean
+  cols?: number
+}
+
+export interface BoardDetails {
+  id: number;
+  user_id: string;
+  title: string;
+  is_favorite: boolean;
+  order: number;
+  created_at: string;
+  columns: Column[]
+}
+
+export type newBoard = Pick<Board, 'user_id' | 'title' | 'order' | 'isOptimistic'>
+
+export type ReorderBoard = Pick<Board, 'id' | 'order'>
+
+export type ReorderTask = Pick<Task, 'id' | 'column_id' | 'position'>
