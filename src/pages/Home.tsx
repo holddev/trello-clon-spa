2f007aa84f3b25d04f9de9139fe8b997ef4aa122
Usuario6842 <136459003+Usuario6842@users.noreply.github.com>
2025-07-12 16:17:20 -0400

    fix(Home): update text for clarity and improve SEO integration

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,16 +5,18 @@ import { Header } from "../components/Header"
 import { Footer } from "../components/Footer"
 import { SignedIn, SignedOut, SignInButton } from "@clerk/clerk-react"
 import { Link } from "react-router-dom"
+import { GenerateSEO } from "../components/SEO"
 
 export const Home = () => {
   return (
     <div className="max-w-5xl mx-auto px-2">
+      <GenerateSEO />
       <Header />
       <main className="flex items-center flex-col md:flex-row gap-5 min-h-screen pt-32 md:pt-0">
         <div className="flex flex-col gap-4 sm:gap-5">
-          <h1 className="font-bold text-2xl sm:text-4xl md:text-5xl md:pb-2">Organiza tu trabajo y tu vida con <span className="text-blue-600 whitespace-nowrap">Trello Clon</span></h1>
+          <h1 className="font-bold text-2xl sm:text-4xl md:text-5xl md:pb-2">Organiza tu trabajo y tu día a día con <span className="text-blue-600 whitespace-nowrap">Trello Clon</span></h1>
           <p className="text-pretty text-sm sm:text-base md:text-xl">
-            La herramienta de colaboración visual que libera el potencial de tu equipo para crear proyectos extraordinarios.
+            Una herramienta visual que te ayuda a organizar múltiples tareas y avanzar en tus proyectos, sin complicaciones.
           </p>
           <SignedIn>
             <Button
