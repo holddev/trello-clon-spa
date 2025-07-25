2e4740e6af2a1029a3e78690af8b5d4af026f4c9
Usuario6842 <136459003+Usuario6842@users.noreply.github.com>
2025-07-25 18:30:38 -0400

    fix: correct input component styling for better focus visibility

--- src/components/UI/Input.tsx
+++ src/components/UI/Input.tsx
@@ -8,7 +8,8 @@ export const Input = ({ className, ...rest }: Props) => {
   return (
     <input
       className={cn(
-        "border-0 bg-transparent w-full outline-i-primary/60 px-2 py-1",
+        "border-2 border-transparent outline-none focus:border-i-primary/60",
+        " bg-transparent w-full px-2 py-1",
         "bg-i-background text-i-foreground placeholder:text-i-foreground/50",
         className
       )}
